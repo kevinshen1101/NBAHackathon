c2c70e59209ca9d7b7d5570c7b3e8085b63591c5
Kevin Shen <kevshen@mit.edu>
2018-07-17 23:31:01 -0500

    Test

--- Application/Basketball Analytics Prompt/plus_minus.py
+++ Application/Basketball Analytics Prompt/plus_minus.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
     play_by_play = process_file("play_by_play.txt")
     event_cache = event_codes.set_index(['Event_Msg_Type', 'Action_Type']).to_dict(orient='index')
     sorted_pbp = play_by_play.sort_values(['Game_id', 'Period', 'PC_Time', 'WC_Time', 'Event_Num'], ascending=[True, True, False, True, True])
-    #print("Test")
     #print(game_sample)
     #print(play_by_play)
     plus_minuses = dict()
